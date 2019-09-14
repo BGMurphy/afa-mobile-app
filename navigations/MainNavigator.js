96cb79999afea7d2b5e5d375ff75789be0e8523a
naohashizume <naobridge46@gmail.com>
2019-09-14 14:58:45 -0700

    create general quiz card component

--- navigations/MainNavigator.js
+++ navigations/MainNavigator.js
@@ -9,18 +9,19 @@ import SettingsScreen from '../screens/SettingsScreen';
 import SurveyScreen from '../screens/SurveyScreen';
 import SignUpScreen from '../screens/SignUp';
 import TabBarIcon from '../components/TabBarIcon';
-
+import QuizScreen from '../screens/Quiz';
 
 // look up
 // https://reactnavigation.org/docs/en/stack-navigator.html#routeconfigs
 
 const HomeStack = createStackNavigator(
   {
-    Home: HomeScreen,
-  },{
+    Home: HomeScreen
+  },
+  {
     defaultNavigationOptions: {
       header: null
-    },
+    }
   }
 );
 
@@ -35,68 +36,79 @@ HomeStack.navigationOptions = {
           : 'md-information-circle'
       }
     />
-  ),
+  )
 };
 
 const SettingsStack = createStackNavigator(
   {
-    Settings: SettingsScreen,
-  },{
+    Settings: SettingsScreen
+  },
+  {
     defaultNavigationOptions: {
       header: null
-    },
+    }
   }
 );
 
 SettingsStack.navigationOptions = {
   tabBarLabel: 'Settings',
   tabBarIcon: ({ focused }) => (
-    <TabBarIcon focused={focused} name={Platform.OS === 'ios' ? 'ios-options' : 'md-options'} />
-  ),
+    <TabBarIcon
+      focused={focused}
+      name={Platform.OS === 'ios' ? 'ios-options' : 'md-options'}
+    />
+  )
 };
 
-const TabNavigator = createBottomTabNavigator(
-  {
-    HomeStack,
-    SettingsStack,
-  }
-)
+const TabNavigator = createBottomTabNavigator({
+  HomeStack,
+  SettingsStack
+});
 
 const SurveyStack = createStackNavigator(
   {
     Survey: {
-      screen: SurveyScreen, navigationOptions: {
-        header: null,
-      },
+      screen: SurveyScreen,
+      navigationOptions: {
+        header: null
+      }
     }
-  },{
+  },
+  {
     defaultNavigationOptions: {
       header: null
-    },
+    }
   }
 );
 
-const AppStack = createStackNavigator({
-  Tab: {
-    screen: TabNavigator
-  }, Survey: {
-    screen: SurveyStack, 
-  }
-},{
-  defaultNavigationOptions: {
-    header: null
+const AppStack = createStackNavigator(
+  {
+    Tab: {
+      screen: TabNavigator
+    },
+    Survey: {
+      screen: SurveyStack
+    }
   },
-});
+  {
+    defaultNavigationOptions: {
+      header: null
+    }
+  }
+);
 // const AppStack = createStackNavigator({ Home: HomeScreen });
-const AuthStack = createStackNavigator({
-  Signup: {
-    screen: SignUpScreen,
-  }
-},{
-  defaultNavigationOptions: {
-    header: null
+const AuthStack = createStackNavigator(
+  {
+    Signup: {
+      screen: SignUpScreen
+    }
   },
-});
+  {
+    defaultNavigationOptions: {
+      header: null
+    }
+  }
+);
 
 const MainNavigator = createAppContainer(
   createSwitchNavigator(
