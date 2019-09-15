817bf88a449d12316f59674ba19c04476d47b6b4
naohashizume <naobridge46@gmail.com>
2019-09-14 17:21:35 -0700

    added picture card in Home Screen

--- screens/Home.js
+++ screens/Home.js
@@ -1,7 +1,16 @@
 import React from 'react';
-import { View, StyleSheet, TouchableOpacity, TextInput, FlatList } from 'react-native';
+import {
+  View,
+  StyleSheet,
+  TouchableOpacity,
+  TextInput,
+  FlatList,
+  ImageBackground
+} from 'react-native';
 import {
   Container,
+  Card,
+  CardItem,
   Header,
   Body,
   Title,
@@ -15,6 +24,12 @@ import {
 import * as Progress from 'react-native-progress';
 import Database from '../config/database';
 import firebase from 'firebase';
+import { LinearGradient } from 'expo-linear-gradient';
+import Dimensions from 'Dimensions';
+const HeaderColor = ['#0818A8', '#024FA8', '#2E96C7'];
+const Color = ['#531CBA', '#0818A8', '#024FA8'];
+let ScreenHeight = Dimensions.get('window').height;
+let ScreenWidth = Dimensions.get('window').width;
 
 // look up
 // https://github.com/oblador/react-native-progress
@@ -37,13 +52,12 @@ export default class Home extends React.Component {
         }
       ]
     };
-
   }
 
   componentDidMount() {
     const { currentUser } = firebase.auth();
-    console.log('currentUser', currentUser)
-    this.setState({ currentUser })
+    console.log('currentUser', currentUser);
+    this.setState({ currentUser });
   }
 
   makeSelectProgram(index) {
@@ -51,46 +65,146 @@ export default class Home extends React.Component {
       const { programs } = this.state;
       this.props.navigation.navigate('Calendar', {
         programId: programs[index].id
-      })
-    }
+      });
+    };
   }
 
   render() {
-    const {currentUser} = this.state;
+    const { currentUser } = this.state;
     return (
-      <View style={styles.container}>
-        <Text>
-          Hi{' '}
-          <Text style={{ fontSize: 20, color: '#0818A8' }}>
-            {currentUser && currentUser.email}!
-          </Text>
-        </Text>
-        <Text>
-          Our Programs:
-        </Text>
-
-        <FlatList
-          data={this.state.programs}
-          keyExtractor={(item, index) => index.toString()}
-          renderItem={({ item, index }) =>
+      <View style={{ flex: 1 }}>
+        <LinearGradient colors={HeaderColor} style={styles.gradient}>
+          <Text style={{ color: '#fff', fontSize: 20 }}>Our Programs</Text>
+        </LinearGradient>
+        <View
+          style={{
+            flex: 1,
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center'
+          }}
+        >
+          <View
+            style={{
+              justifyContent: 'center',
+              alignItems: 'center'
+            }}
+          >
             <Text
-              onPress={this.makeSelectProgram(index)}>{item.name}</Text>}
-        />
+              style={{
+                alignSelf: 'center',
+                fontSize: 20,
+                justifyContent: 'center',
+                alignItems: 'center'
+              }}
+            >
+              Hi,{' '}
+              <Text style={{ fontSize: 20, color: '#0818A8' }}>
+                {currentUser && currentUser.email}!
+              </Text>
+            </Text>
+
+            <FlatList
+              data={this.state.programs}
+              keyExtractor={(item, index) => index.toString()}
+              renderItem={({ item, index }) => (
+                <ImageBackground
+                  source={require('../assets/programs-aquavision.jpg')}
+                  style={styles.img}
+                >
+                  <LinearGradient colors={Color} style={styles.gradientCard}>
+                    <View
+                      style={{
+                        width: ScreenWidth - 20,
+                        height: ScreenHeight - 450,
+
+                        marginBottom: 20,
+                        borderRadius: 20
+                      }}
+                    >
+                      <Text onPress={this.makeSelectProgram(index)}>
+                        {item.name}
+                      </Text>
+                    </View>
+                  </LinearGradient>
+                </ImageBackground>
+              )}
+            />
+          </View>
+        </View>
       </View>
+
+      // <View style={{ flex: 1 }}>
+      //   <Container style={styles.container}>
+      //     <LinearGradient colors={Color} style={styles.gradient}>
+      //       <Text style={{ color: '#fff', fontSize: 20 }}>Our Programs</Text>
+      //     </LinearGradient>
+      //     <View
+      //       style={{
+      //         flex: 1,
+      //         flexDirection: 'column',
+      //         justifyContent: 'center',
+      //         alignItems: 'center'
+      //       }}
+      //     >
+      //       <Text style={{ textAlign: 'center' }}>
+      //         Hi{' '}
+      //         <Text style={{ fontSize: 20, color: '#0818A8' }}>
+      //           {currentUser && currentUser.email}!
+      //         </Text>
+      //       </Text>
+
+      //       <FlatList
+      //         data={this.state.programs}
+      //         keyExtractor={(item, index) => index.toString()}
+      //         renderItem={({ item, index }) => (
+      //           <Card>
+      //             <CardItem>
+      //               <Body>
+      //                 <Text onPress={this.makeSelectProgram(index)}>
+      //                   {item.name}
+      //                 </Text>
+      //               </Body>
+      //             </CardItem>
+      //           </Card>
+      //         )}
+      //       />
+      //     </View>
+      //   </Container>
+      // </View>
     );
   }
 }
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: 'column',
-    backgroundColor: 'lavender',
-    justifyContent: 'center',
-    alignItems: 'center'
+    backgroundColor: 'lavender'
   },
   signOutButton: {
     alignSelf: 'center',
     textAlign: 'center',
     width: '90%'
+  },
+
+  gradient: {
+    width: ScreenWidth,
+    opacity: 0.8,
+    height: 70,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingTop: 10
+  },
+  gradientCard: {
+    opacity: 0.7,
+    marginBottom: '5%',
+    borderRadius: 20
+  },
+  img: {
+    resizeMode: 'contain',
+    width: ScreenWidth - 20,
+    height: ScreenHeight - 450,
+    marginBottom: 20,
+    borderRadius: 20
   }
 });
